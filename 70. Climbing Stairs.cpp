d0d5dfb20fb9b59f06cac8a53b5c0de70fa0b0c5
Leo Wang <star901799@gmail.com>
2021-07-21 20:30:51 +0800

    Create 70. Climbing Stairs.cpp

---
+++ 70. Climbing Stairs.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if (n == 1) return 1;
+        
+        int a = 1;
+        int b = 2;
+        int c;
+        for (int i = 3; i <= n; ++i) {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        return b;
+        
+        
+        
+    }
+};
